d5821746b58e69fd36367ac8032dc18661148db2
htchepannou <htchepannou@expedia.com>
2021-05-12 19:18:32 -0400

    update email sdk

--- src/main/kotlin/com/wutsi/email/EmailApi.kt
+++ src/main/kotlin/com/wutsi/email/EmailApi.kt
@@ -1,6 +1,7 @@
 package com.wutsi.email
 
 import com.wutsi.email.dto.SendEmailRequest
+import feign.Headers
 import feign.Param
 import feign.RequestLine
 import kotlin.Long
@@ -9,9 +10,11 @@ import kotlin.Unit
 
 public interface EmailApi {
   @RequestLine("POST /v1/emails")
+  @Headers("Content-Type: application/json")
   public fun send(request: SendEmailRequest): Unit
 
   @RequestLine("DELETE /v1/sites/{site-id}/list/members?email={email}&user-id={user-id}")
+  @Headers("Content-Type: application/json")
   public fun unsubscribe(
     @Param("site-id") siteId: Long,
     @Param("email") email: String,
